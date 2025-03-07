0f2e53a4b625f8a4de8172a4462cafe28ceceeb6
Tarun Dubey <tarun2019dubey@gmail.com>
2025-03-07 12:16:08 +0530

    Tarun add. view by city and state

--- addressBook.js
+++ addressBook.js
@@ -25,6 +25,27 @@ class AddressBook{
     searchByState(state) {
         return this.contacts.filter(contact => contact.state.toLowerCase() === state.toLowerCase());
     }
+        // 🔹 New: View persons by City
+        viewPersonsByCity(city) {
+            let peopleInCity = this.searchByCity(city);
+            if (peopleInCity.length > 0) {
+                console.log(`People in ${city}:`);
+                peopleInCity.map(contact => console.log(`${contact.firstName} ${contact.lastName}`));
+            } else {
+                console.log(`No contacts found in ${city}.`);
+            }
+        }
+    
+        // 🔹 New: View persons by State
+        viewPersonsByState(state) {
+            let peopleInState = this.searchByState(state);
+            if (peopleInState.length > 0) {
+                console.log(`People in ${state}:`);
+                peopleInState.map(contact => console.log(`${contact.firstName} ${contact.lastName}`));
+            } else {
+                console.log(`No contacts found in ${state}.`);
+            }
+        }
 
 }
 module.exports=AddressBook;
